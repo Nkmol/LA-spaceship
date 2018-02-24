a94f2e2362be7d62cc67244d15388ba79b14aa7c
Sander Mol <Sandermol95@hotmail.com>
2018-02-24 22:47:33 +0100

    Add camera movement

--- la-Spaceship.Runnable/main.cpp
+++ la-Spaceship.Runnable/main.cpp
@@ -86,9 +86,71 @@ int main(int argc, char *argv[]) {
 					running = false;
 				}
 
+				// TODO use the current camera angel to calculate the movement?
 				if (inputHandler.is_key_pressed(InputHandler::keys::KEY_UP_MOVE))
 				{
-					std::cout << "UP" << std::endl;
+					std::cout << "Moving up" << std::endl;
+					cube = factory.CreateTranslationMatrix(0, 1, 0) * cube;
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_DOWN_MOVE))
+				{
+					std::cout << "Moving down" << std::endl;
+					cube = factory.CreateTranslationMatrix(0, -1, 0) * cube;
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_LEFT_MOVE))
+				{
+					std::cout << "Moving left" << std::endl;
+					cube = factory.CreateTranslationMatrix(-1, 0, 0) * cube;
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_RIGHT_MOVE))
+				{
+					std::cout << "Moving right" << std::endl;
+
+				/*	try
+					{
+						double collective_x = 0;
+						double collective_y = 0;
+						double collective_z = 0;
+
+						for (int i = 0; i < cube.GetRow(0).size(); i++)
+						{
+							collective_x += cube.GetVal(0, i);
+							collective_y += cube.GetVal(1, i);
+							collective_z += cube.GetVal(2, 1);
+						}
+
+						double center_x = collective_x / cube.GetRow(0).size();
+						double center_y = collective_y / cube.GetRow(1).size();
+						double center_z = collective_z / cube.GetRow(2).size();
+					}
+					catch(...)
+					{
+						
+					}
+*/
+					cube = factory.CreateTranslationMatrix(1, 0, 0) * cube;
+				}
+
+				if (inputHandler.is_key_pressed(InputHandler::keys::KEY_UP_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving up" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(0, 1, 0) * camera.GetEye());
+
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_DOWN_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving down" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(0, -1, 0) * camera.GetEye());
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_LEFT_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving left" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(-1, 0, 0) * camera.GetEye());
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_RIGHT_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving right" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(1, 0, 0) * camera.GetEye());
 				}
 			}
 
