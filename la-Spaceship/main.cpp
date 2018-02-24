fbc5d02e2ee38a6e446c2ba97fde51717bfd8891
jonathanVoort <jjvoort@avans.nl>
2018-02-24 18:20:19 +0100

    Created spaceship
    TODO:
    - finish rotation

--- la-Spaceship/main.cpp
+++ la-Spaceship/main.cpp
@@ -14,7 +14,7 @@
 #undef main
 int main(int argc, char *argv[]) {
 	// init through a initalizer_list
-	const Matrix<double, 4, 16> cube(
+	Matrix<double, 4, 16> cube(
         {
             {100, 150, 150, 150, 150, 100, 100, 100, 100, 100, 150, 150, 150, 150, 100, 100},
             {100, 100, 100, 100, 150, 150, 150, 150, 100, 100, 100, 150, 150, 150, 150, 100},
@@ -23,7 +23,67 @@ int main(int argc, char *argv[]) {
         }
     );
 
+	Matrix<double, 4, 16> engine(
+		{
+			{0, 0, 0, 0, 0, 40, 40, 0, 40, 40, 0, 40, 40, 0, 40, 40},
+			{0, 0, 100, 100, 0, 10, 10, 0, 10, 90, 100, 90, 90, 100, 90, 10},
+			{30, 110, 110, 30, 30, 30, 110, 110, 110, 110, 110, 110, 30, 30, 30, 30},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+		}
+	);
+
+	Matrix<double, 4, 16> body(
+		{
+			{40, 40, 140, 40, 40, 140, 40, 40, 140, 40, 40, 140, 140, 140, 140, 140},
+			{10, 10, 10, 10, 90, 90, 90, 90, 90, 90, 10, 10, 10, 90, 90, 10},
+			{30, 110, 110, 110, 110, 110, 110, 30, 30, 30, 30, 30, 110, 110, 30, 30},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+		}
+	);
+
+	Matrix<double, 4, 8> head(
+		{
+			{200, 140, 200, 140, 200, 140, 200, 140},
+			{50, 10, 50, 90, 50, 90, 50, 10},
+			{70, 30, 70, 110, 70, 30, 70, 110},
+			{1, 1, 1, 1, 1, 1, 1, 1}
+		}
+	);
+
+	Matrix<double, 4, 3> left_wing(
+		{
+			{40, 40, 140},
+			{10, 10, 10},
+			{40, 0, 40},
+			{1,  1, 1 }
+		}
+	);
+	Matrix<double, 4, 3> right_wing(
+	{
+		{ 40, 40, 140 },
+		{ 10, 10, 10 },
+		{ 100, 140, 100 },
+		{ 1,  1, 1 }
+	}
+	);
+
+	Matrix<double, 4, 6> source_edges(
+		{
+			{0, 1000, 0, 0, 0, 0},
+			{0, 0, 0, 1000, 0, 0},
+			{0, 0, 0, 0, 0, 1000},
+			{1 ,1, 1, 1, 1, 1   }
+		}
+	);
+
 	Object test_object;
+	Object source_object;
+
+	Object engine_object;
+	Object body_object;
+	Object head_object;
+	Object left_wing_object;
+	Object right_wing_object;
 
 	auto m = test_object.ToMatrix<4>();
 	test_object.FromMatrix(m);
@@ -33,15 +93,15 @@ int main(int argc, char *argv[]) {
 
 	Camera camera { 
 		 {
-            {0},
+            {100},
+            {150},
             {250},
-            {0},
             {1}
         },
         {
-            {250},
+            {100},
             {0},
-            {250},
+            {0},
             {1}
         },
 		{
@@ -51,8 +111,6 @@ int main(int argc, char *argv[]) {
 			{1}
 		}
 	};
-
-	const auto projectedMatrix = camera.ProjectMatrix(cube);
 
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -77,18 +135,127 @@ int main(int argc, char *argv[]) {
 					running = false;
 				}
 
+				// TODO use the current camera angel to calculate the movement?
 				if (inputHandler.is_key_pressed(InputHandler::keys::KEY_UP_MOVE))
 				{
 					std::cout << "Moving up" << std::endl;
+					cube = factory.CreateTranslationMatrix(0, 1, 0) * cube;
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_DOWN_MOVE))
+				{
+					std::cout << "Moving down" << std::endl;
+					cube = factory.CreateTranslationMatrix(0, -1, 0) * cube;
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_LEFT_MOVE))
+				{
+					std::cout << "Moving left" << std::endl;
+					cube = factory.CreateTranslationMatrix(-1, 0, 0) * cube;
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_RIGHT_MOVE))
+				{
+					std::cout << "Moving right" << std::endl;
+
+					double center_x;
+					double center_y;
+					double center_z;
+
+					try
+					{
+						double collective_x = 0;
+						double collective_y = 0;
+						double collective_z = 0;
+
+						for (int i = 0; i < cube.Getrow(0).size(); i++)
+						{
+							collective_x += cube.Getval(0, i);
+							collective_y += cube.Getval(1, i);
+							collective_z += cube.Getval(2, 1);
+						}
+
+						center_x = collective_x / cube.Getrow(0).size();
+						center_y = collective_y / cube.Getrow(1).size();
+						center_z = collective_z / cube.Getrow(2).size();
+					}
+					catch(...)
+					{
+						
+					}
+
+					cube = factory.CreateTranslationMatrix(1, 0, 0) * cube;
+				}
+
+				if (inputHandler.is_key_pressed(InputHandler::keys::KEY_UP_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving up" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(0, 1, 0) * camera.GetEye());
+
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_DOWN_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving down" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(0, -1, 0) * camera.GetEye());
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_LEFT_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving left" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(-1, 0, 0) * camera.GetEye());
+				}
+				else if (inputHandler.is_key_pressed(InputHandler::keys::KEY_RIGHT_CAMERA_MOVE))
+				{
+					std::cout << "Camera Moving right" << std::endl;
+					camera.SetEye(factory.CreateTranslationMatrix(1, 0, 0) * camera.GetEye());
 				}
 			}
+
+			const auto projectedMatrix = camera.ProjectMatrix(cube);
+			const auto projected_source_edges = camera.ProjectMatrix(source_edges);
+
+			const auto projected_engine = camera.ProjectMatrix(engine);
+			const auto projected_body = camera.ProjectMatrix(body);
+			const auto projected_head = camera.ProjectMatrix(head);
+			const auto projected_left_wing = camera.ProjectMatrix(left_wing);
+			const auto projected_right_wing = camera.ProjectMatrix(right_wing);
+
 
 			test_object.FromMatrix(
 				projectedMatrix
 			);
 
+			source_object.FromMatrix(
+				projected_source_edges
+			);
+
+			engine_object.FromMatrix(
+				projected_engine
+			);
+
+			body_object.FromMatrix(
+				projected_body
+			);
+
+			head_object.FromMatrix(
+				projected_head
+			);
+
+			left_wing_object.FromMatrix(
+				projected_left_wing
+			);
+
+			right_wing_object.FromMatrix(
+				projected_right_wing
+			);
+
 			RenderManager::GetInstance().Clear();
-			test_object.Draw();
+			//test_object.Draw();
+
+			// Spaceship
+			engine_object.Draw();
+			body_object.Draw();
+			head_object.Draw();
+			left_wing_object.Draw();
+			right_wing_object.Draw();
+
+		//	source_object.Draw();
 			RenderManager::GetInstance().Refresh();
 		}
 	}
