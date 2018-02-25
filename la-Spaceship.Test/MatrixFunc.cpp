791c707284e49478c9170293fdc3cbc09ba926d6
Sander Mol <Sandermol95@hotmail.com>
2018-02-25 23:53:44 +0100

    Add dot test

--- la-Spaceship.Test/MatrixFunc.cpp
+++ la-Spaceship.Test/MatrixFunc.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include "../la-Spaceship/Object.h"
 #include "../la-Spaceship/Models.h"
+#include "../la-Spaceship/MatrixHelper.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -51,7 +52,7 @@ namespace laSpaceshipTest
 				/*y*/{0, 0, 5, 5, 0, 5, 5, 0},
 				/*z*/{0, 0, 0, 0, 5, 5, 5, 5},
 
-				/*w*/{5, 5, 5, 5, 5, 5, 5, 5}
+				/*w*/{1, 1, 1, 1, 1, 1, 1, 1}
 			};
 			Object object ({0, 0, 0}, {1, 1, 1}, cube);
 
@@ -67,6 +68,66 @@ namespace laSpaceshipTest
 			Assert().AreEqual(center.GetVal(2, 0), 2.5);
 			Assert().AreEqual(center.GetVal(3, 0), 1.0);
 		}
+
+		TEST_METHOD(dot_2x2)
+		{
+			// Arrange
+			MatrixHelper helper;
+			const Matrix<double, 4, 1> vec1{
+				/*x*/{2},
+				/*y*/{2},
+				/*z*/{2},
+
+				/*w*/{1}
+			};
+			
+			const Matrix<double, 4, 1> vec2{
+				/*x*/{2},
+				/*y*/{2},
+				/*z*/{2},
+
+				/*w*/{1}
+			};
+
+			// Act
+			const auto result = helper.Dot(vec1, vec2);
+
+			// Assert
+			Assert().AreEqual(result, 12.0);
+		}
+
+		//TEST_METHOD(cross_5x1_7x1_should_give_2x1_differnce)
+		//{
+		//	// Arrange
+		//	MatrixHelper helper;
+		//	const Matrix<double, 4, 1> vec1{
+		//		/*x*/{5},
+		//		/*y*/{5},
+		//		/*z*/{5},
+
+		//		/*w*/{1}
+		//	};
+		//	
+		//	const Matrix<double, 4, 1> vec2{
+		//		/*x*/{7},
+		//		/*y*/{7},
+		//		/*z*/{7},
+
+		//		/*w*/{1}
+		//	};
+
+		//	// Act
+		//	const auto result = helper.Cross(vec2, vec1);
+
+		//	// Assert
+		//	Assert().AreEqual(static_cast<int>(result.GetCol(0).size()), 4);
+		//	Assert().AreEqual(static_cast<int>(result.GetRow(0).size()), 1);
+
+		//	Assert().AreEqual(result.GetVal(0, 0), 2.0);
+		//	Assert().AreEqual(result.GetVal(1, 0), 2.0);
+		//	Assert().AreEqual(result.GetVal(2, 0), 2.0);
+		//	Assert().AreEqual(result.GetVal(3, 0), 1.0);
+		//}
 	private:
 		static Matrix<double, 4, 4> CreateTestMatrix()
 		{
